7c3accaca89a89c8a40da2cbf73f2882d5b012f5
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-02 09:20:14 -0500

    :wrench: CHORE: Configuración Springboot.

---
+++ src/main/java/com/turismo/service_microservice/ServiceMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.turismo.service_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turismo/service_microservice/ServiceMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turismo.service_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
